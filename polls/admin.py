8a9bb74901ce3756f309cb351acdbf2a38ca5723
kartik-25 <k.p.2511998@gmail.com>
2017-06-21 07:33:28 +0530

    Customised admin site

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,19 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
 
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    list_display = ('question_text', 'pub_date')
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
