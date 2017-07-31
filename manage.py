2d20452bda7cd5ee39d338bd052002960b7c5ad3
kartik-25 <pandeykartikey99@gmail.com>
2017-07-31 13:00:24 +0530

    Minor changes

--- manage.py
+++ manage.py
@@ -4,10 +4,6 @@ import sys
 import re
 
 def read_env():
-    """Pulled from Honcho code with minor updates, reads local default
-    environment variables from a .env file located in the project root
-    directory.
-    """
     try:
         with open('.env') as f:
             content = f.read()
@@ -15,15 +11,9 @@ def read_env():
         content = ''
 
     for line in content.splitlines():
-        m1 = re.match(r'\A([A-Za-z_0-9]+)=(.*)\Z', line)
+        m1 = re.match(r'\A(.*)=(.*)\Z', line)
         if m1:
             key, val = m1.group(1), m1.group(2)
-            m2 = re.match(r"\A'(.*)'\Z", val)
-            if m2:
-                val = m2.group(1)
-            m3 = re.match(r'\A"(.*)"\Z', val)
-            if m3:
-                val = re.sub(r'\\(.)', r'\1', m3.group(1))
             os.environ.setdefault(key, val)
 
 
--- polls/tests.py
+++ polls/tests.py
@@ -17,10 +17,10 @@ class QuestionModelTests(TestCase):
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
 
-	def test_was_published_recently_with_recent_question(self):
-		time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-		recent_question = Question(pub_date=time)
-		self.assertIs(recent_question.was_published_recently(), True)
+    def test_was_published_recently_with_recent_question(self):
+	    time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+	    recent_question = Question(pub_date=time)
+	    self.assertIs(recent_question.was_published_recently(), True)
 
 def create_question(question_text, days):
 	time = timezone.now() + datetime.timedelta(days=days)
